e85c838df775487fed66ded399ce6dc80d73b008
lfvaldezit <lfvaldez.it@gmail.com>
2025-04-22 10:51:59 -0300

    R53 module commit

--- modules/r53/resolver-endpoint/outputs.tf
+++ modules/r53/resolver-endpoint/outputs.tf
@@ -1,3 +1,7 @@
 output "resolver_endpoint_id" {
   value = aws_route53_resolver_endpoint.this.id
 }
+
+output "ip_address" {
+  value = aws_route53_resolver_endpoint.this.ip_address
+}
---
+++ modules/r53/resolver-rule/outputs.tf
@@ -0,0 +1,7 @@
+output "resolver_rule_id" {
+  value = aws_route53_resolver_rule.this.id
+}
+
+output "resolver_arn" {
+  value = aws_route53_resolver_rule.this.arn
+}
